0afb410e101738c5e5ee3674fb730ecc51dc82c3
VÕ DUY TÂN . <voduytan2004@gmail.com>
2025-07-24 16:27:18 +0700

    Hien thi cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
+import './assets/main.css';
+import router from './router';
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(router);
 
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
